ad220d8f7754ee9421649ee0f5e14af29fff0f07
tetsumote <unionproject88@gmail.com>
2023-01-22 10:21:22 +0100

    umieszczanie kodu

---
+++ umieszczanieSkryptu/script.js
@@ -0,0 +1,28 @@
+// Utwórz plik index.html i script.js dodaj link do pliku w tagu HEAD
+
+
+// Utwórz plik index.html i umieść tag script wewnątrz tagu BODY
+
+
+// Utwórz plik index.html i umieść tag script wewnątze tagu HEAD
+
+
+// Utwórz plik index.html i podłącz do niego 2 plki js znajdujące się w tym samym katalogu
+
+
+// Utwórz plik index.html i dodaj dwa tagi script w HEAD i BODY
+
+
+// Utwórz plik index.html i katalog script w nim 2 pliki JS podłacz je w tagu HEAD
+
+
+// Utwórz plik index.html i dodaj script z CDN
+
+
+// Utwórz plik index.html i dodaj tag script w HEAD i BODY
+
+
+// Dadaj tag script z console.log("Test") poza tagiem HTML i sprawdź czy działą
+
+
+// Dodaj tag script z console.log("Test") poniżej tagu BODY i sprawdź czy działą
